de07a6346bf41dce8e4a3e367077122f566fece3
aadeshkolhe <aadeshkolhe>
2025-06-28 11:59:05 +0530

    Initial commit

---
+++ src/test/java/com/working/app_service_registry/AppServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.working.app_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
